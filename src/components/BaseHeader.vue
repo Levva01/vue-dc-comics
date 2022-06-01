0b6c4ec758e83bdd20b12cf32419bc44742f184f
Levva01 <vellanifederico13@gmail.com>
2022-06-01 15:55:28 +0200

    header inserimento nav ed immagine

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,25 @@
 <template>
-  <header>
-      
+  <header class="header">
+
+      <div class="header__logo">
+          <img src="../assets/img/dc-logo.png" alt="">
+      </div>
+
+      <div class="header__nav">
+          <ul>
+              <li><a href="#">CHARACTERS</a></li>
+              <li><a href="#">COMICS</a></li>
+              <li><a href="#">MOVIES</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">GAMES</a></li>
+              <li><a href="#">COLLECTIBLES</a></li>
+              <li><a href="#">VIDEOS</a></li>
+              <li><a href="#">FANS</a></li>
+              <li><a href="#">NEWS</a></li>
+              <li><a href="#">SHOP</a></li>
+          </ul>
+      </div>
+
   </header>
 </template>
 
@@ -13,6 +32,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.header{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 120px;
+
+    &__logo {
+        img {
+            width: 80px;
+            height: auto;
+        }
+    }
+
+    &__nav {
+        ul{
+            list-style: none;
+            display: flex;
+            justify-content: center;
+            gap: .9375rem;
+            font-size: .875rem;
+            font-weight: bold;
+
+            a{
+                text-decoration: none;
+                color: #464646;
+            }
+        }
+
+    }
+
+
+}
 
 </style>
